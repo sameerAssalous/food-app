7aa5b8bdece4fd3fded80d2d1ec57310a48f9e6c
sameerAssalous <sam_sal_2010@hotmail.com>
2022-10-05 06:18:23 +0200

    add Order resource

---
+++ Modules/Order/Resources/OrderResource.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Module\Order\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'status' => $this->status,
+            'products' => ProductResource::collection($this->products),
+            'created_at' => $this->created_at,
+        ];
+    }
+}
